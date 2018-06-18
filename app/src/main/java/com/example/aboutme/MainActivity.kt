665d67bb500164d5cd0f0b1420c5ade52b1839ae
alekshaecky <vhaecky@google.com>
2018-06-18 16:01:06 -0700

    Lots of fixes from Jose and others.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,23 +5,26 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_done.setOnClickListener {
+            addNickname(it)
+        }
     }
 
     fun addNickname(view : View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = findViewById<TextView>(R.id.nickname)
-        //val doneButton = findViewById<Button>(R.id.done_button)
 
         nicknameTextView.text = editText.text
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nicknameTextView.visibility = View.VISIBLE
     }
-
 }
