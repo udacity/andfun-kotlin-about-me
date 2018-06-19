9e51804a54e2b7de16fd62aac955bce291d1f8ef
alekshaecky <vhaecky@google.com>
2018-06-19 13:48:09 -0700

    Add data binding for actual data. Add comments.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,14 +7,40 @@ import android.view.View
 import com.example.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Main Activity of the AboutMe app. This final app version demonstrates:
+ *     * Using a LinearLayout with TextViews and EditText
+ *     * Using a click handler with a button
+ *     * Setting the visibility status of a view
+ *     * Getting user input with an EditText and using the inputted value
+ *     * Creating and using data biding to get rid of findViewById
+ *     * Creating and using data binding to set the text of a text view
+ */
 class MainActivity : AppCompatActivity() {
 
+    // Binding object for MainActivity.
+    // Name of the object is derived from the name of the activity or fragment.
     private lateinit var binding: ActivityMainBinding
+
+    // Instance of MyName. Since we are using it in multiple places, make it a member variable.
+    private var newName: MyName = MyName()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Setting the content view using the DataBindingUtil creates an instance of
+        // ActivityMainBinding from the supplied activity and the supplied layout. This object
+        // contains mappings between the activity and layout, and some magic to interact with them.
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // Create an instance of MyName and assign values.
+        newName.name = "Borsov"
+        // Give this a name that's clearly not what we want to end up with, so we can see how
+        // it changes.
+        newName.nickname = "placeholder" // Otherwise null and app crashes.
+        // This sets the value of the aboutMe that is declared and used in the layout file.
+        binding.aboutMe = newName
+
+        // Just a plain old click listener like we set in the previous lesson.
         button_done.setOnClickListener {
             addNickname(it)
         }
@@ -26,9 +52,17 @@ class MainActivity : AppCompatActivity() {
      * by eliminating calls to findViewById.
      */
     fun addNickname(view : View) {
-        binding.nickname.text = binding.nicknameEdit.text
+        newName.nickname = binding.nicknameEdit.text.toString()
+
+        // And we change which views are visible.
+        // Remove the EditText and the Button. With GONE they are invisible and do not occupy space.
         binding.nicknameEdit.visibility = View.GONE
-        view.visibility = View.GONE
+        binding.buttonDone.visibility = View. GONE
+        // Since the button is passed in, we could reference it like this.
+        // view.visibility = View.GONE
+        // but it's more readable and consistent to use "binding." for all views.
+
+        // Now make the text view with the nickname visible.
         binding.nickname.visibility = View.VISIBLE
     }
 }
---
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,10 @@
+package com.example.aboutme
+
+/**
+ * Container for a name and a nickname string.
+ * Used to demonstrate data binding.
+ */
+class MyName  {
+    lateinit var name : String
+    lateinit var nickname : String
+}
