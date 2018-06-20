6faf4cce97f4e40597781ebbad481fde2cf93c9d
alekshaecky <vhaecky@google.com>
2018-06-20 10:46:34 -0700

    Tweaking and polishing. Edited nickname does not show.

--- app/src/main/java/com/example/aboutme/MyName.kt
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -1,10 +1,10 @@
 package com.example.aboutme
 
 /**
- * Container for a name and a nickname string.
+ * Container for a name and a nickname strings.
  * Used to demonstrate data binding.
  */
-class MyName  {
-    lateinit var name : String
-    lateinit var nickname : String
+data class MyName(
+        var name: String = "",
+        var nickname: String = "") {
 }
