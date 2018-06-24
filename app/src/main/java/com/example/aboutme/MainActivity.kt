8ac61cc44f4f40f3bb00ccde3a0b1a5de6b536ab
Marina Tanasyuk <marina.tanasyuk@gmail.com>
2018-06-24 11:28:40 -0700

    Some small nits

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.aboutme
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,18 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_done.setOnClickListener {
+        doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     fun addNickname(view : View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
         view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        nicknameText.visibility = View.VISIBLE
     }
 }
