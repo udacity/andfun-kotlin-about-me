bf58a7ea67799e083bb230a57839fc2de049e7e6
assersamak <mr.fuzzz@gmail.com>
2018-08-13 18:40:27 +1000

    Step.02-Exercise-Add-TextView-ImageView-Style

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -34,5 +34,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO (02) Connect the layout to the activity using setContentView()
     }
 }
