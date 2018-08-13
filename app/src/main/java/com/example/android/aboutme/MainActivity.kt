e300602238757829b7726dfb67029730eb730939
assersamak <mr.fuzzz@gmail.com>
2018-08-13 19:44:17 +1000

    Step.05-Exercise-Implement-data-binding

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -38,17 +38,27 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    // TODO (03) Create a binding object in the MainActivity.
+
+    // TODO (09) Create an instance of MyName and set binding.myName to it.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO (04) Use DataBindingUtil to set the layout for MainActivity.
         setContentView(R.layout.activity_main)
-
+        // TODO (05) Use the binding object to replace all calls to findViewById.
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
-
     }
 
+    /**
+     * Click handler for the Done button.
+     * Demonstrates how data binding can be used to make code much more readable
+     * by eliminating calls to findViewById and changing data in the binding object.
+     */
     private fun addNickname(view: View) {
+        // TODO (10) set the nickname in the binding object and invalidateAll().
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
@@ -61,5 +71,4 @@ class MainActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
-
 }
