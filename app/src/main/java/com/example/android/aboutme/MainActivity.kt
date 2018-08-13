0bdfad3768a46e675003af22ce6223f8f3059adc
assersamak <mr.fuzzz@gmail.com>
2018-08-13 17:47:11 +1000

    Step.01-Exercise-Create-layout-file

---
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.aboutme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+ *     * ScrollView to display scrollable text
+ *     * Getting user input with an EditText.
+ *     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
+ *     * Setting the visibility status of a view.
+ *     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+ *       and how to display data in views using the data binding object.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
