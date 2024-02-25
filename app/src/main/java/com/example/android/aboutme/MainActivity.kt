838afe02958f95f013100f47dc0bc8a1102f24e7
Hemant Shori <hemantshori93@gmail.com>
2024-02-25 22:28:22 +0100

    Add Text Image using Compose UI

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.android.aboutme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android.aboutme.ui.theme.AboutMeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,16 +39,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier
+                .padding(top = 8.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.baseline_star_border_24),
+            contentDescription = "Star",
+            modifier = Modifier
+                .size(height = 48.dp, width = 48.dp)
+                .padding(top = 8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AboutMeTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting(
+                "About me"
+            )
+        }
     }
 }
