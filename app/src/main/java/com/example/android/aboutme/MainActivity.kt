44dbb809136d3eae3348ad0b075ee4b244de2f5c
Hemant Shori <hemantshori93@gmail.com>
2024-02-25 21:47:13 +0100

    Update gradle and create layout with ComposeUI

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,87 +1,46 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.aboutme
 
-import android.content.Context
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import com.example.android.aboutme.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android.aboutme.ui.theme.AboutMeTheme
 
-
-/**
- * Main Activity of the AboutMe app. This app demonstrates:
- *     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
- *     * ScrollView to display scrollable text
- *     * Getting user input with an EditText.
- *     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
- *     * Setting the visibility status of a view.
- *     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
- *       and how to display data in views using the data binding object.
- */
-class MainActivity : AppCompatActivity() {
-
-    // Binding object for MainActivity.
-    // Name of the object is derived from the name of the activity or fragment.
-    private lateinit var binding: ActivityMainBinding
-
-    // Instance of MyName data class.
-    private val myName: MyName = MyName("Aleks Haecky")
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Setting the content view using DataBindingUtil creates an instance of
-        // ActivityMainBinding from the supplied activity and the supplied layout. This object
-        // contains mappings between the activity and layout,
-        // and functionality to interact with them.
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        // Set the value of the myName variable that is declared and used in the layout file.
-        binding.myName = myName
-
-        // Click listener for the Done button.
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
+        setContent {
+            AboutMeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
     }
+}
 
-    /**
-     * Click handler for the Done button.
-     * Demonstrates how data binding can be used to make code much more readable
-     * by eliminating calls to findViewById and changing data in the binding object.
-     */
-    private fun addNickname(view: View) {
-        binding.apply {
-            // Set the text for nicknameText to the value in nicknameEdit.
-            myName?.nickname = nicknameEdit.text.toString()
-            // Invalidate all binding expressions and request a new rebind to refresh UI
-            invalidateAll()
-            // Change which views are visible.
-            // Remove the EditText and the Button.
-            // With GONE they are invisible and do not occupy space.
-            nicknameEdit.visibility = View.GONE
-            doneButton.visibility = View.GONE
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-            // Make the TexView with the nickname visible.
-            nicknameText.visibility = View.VISIBLE
-        }
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AboutMeTheme {
+        Greeting("Android")
     }
 }
