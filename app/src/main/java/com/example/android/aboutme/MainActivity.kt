10dbb6ade01d0d119f675d6497a632a0122433ed
Hemant Shori <hemantshori93@gmail.com>
2024-03-01 21:48:40 +0100

    Add a TextField-DoneButton-ClickHandler-ComposeUI
    Step.04-Solution-TextField-DoneButton-ClickHandler-ComposeUI

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.aboutme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,18 +13,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android.aboutme.ui.theme.AboutMeTheme
@@ -63,7 +74,8 @@ class MainActivity : ComponentActivity() {
             run {
                 Column(
                     Modifier
-                        .padding(start = 8.dp,
+                        .padding(
+                            start = 8.dp,
                             end = 8.dp,
                             top = innerPadding.calculateTopPadding(),
                             bottom = innerPadding.calculateBottomPadding()
@@ -78,17 +90,60 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun HeaderUiElement(name: String, modifier: Modifier = Modifier) {
+        var nickname by rememberSaveable { mutableStateOf("") }
+        var isNicknameTextVisible by rememberSaveable { mutableStateOf(false) }
         Column(
             modifier = modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
             Text(
                 text = name,
                 style = MaterialTheme.typography.headlineLarge,
+                textAlign = TextAlign.Center,
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(top = 8.dp)
             )
+            // Nickname edittext
+            if (!isNicknameTextVisible) {
+                TextField(
+                    value = nickname,
+                    onValueChange = { newText: String -> nickname = newText },
+                    label = { Text(text = "What is your Nickname?") },
+                    modifier = Modifier.fillMaxWidth(),
+                    textStyle = TextStyle(color = Color.Black),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                )
+            }
+            // Nickname text view
+            AnimatedVisibility(visible = isNicknameTextVisible) {
+                Text(
+                    text = nickname,
+                    style = MaterialTheme.typography.headlineMedium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                )
+            }
+            // Done Button
+            Button(
+                onClick = {
+                    // Do not hide the TextField if nickname is empty
+                    if (nickname.isNotEmpty()) {
+                        isNicknameTextVisible = true
+                    }
+                },
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text(
+                    text = "Done",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                )
+            }
             Image(
                 painter = painterResource(id = R.drawable.baseline_star_border_24),
                 contentDescription = "Star",
